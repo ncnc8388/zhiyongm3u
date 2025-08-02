fa52fff3aa6e4b4a2750a0451e141613f21fd5f7
ncnc8388 <109010398+ncnc8388@users.noreply.github.com>
2025-08-02 11:24:30 +0800

    Update iptv.py

--- .github/workflows/iptv.py
+++ .github/workflows/iptv.py
@@ -251,6 +251,8 @@ def generate_sorted_m3u(valid_urls, cctv_channels, province_channels, filename):
 
     # 写入 M3U 文件
     with open(filename, 'w', encoding='utf-8') as f:
+        for urlxx in vaild_urls:
+            f.write(urlxx + '\n')
         f.write("#EXTM3U\n")
         for channel_info in all_channels:
             f.write(
@@ -332,5 +334,6 @@ if __name__ == "__main__":
     # 执行主函数
 
     asyncio.run(main(file_urls, cctv_channel_file, province_channel_files))
+
 
 
