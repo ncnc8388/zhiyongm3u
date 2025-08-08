b91a3aa54f2e188d0f3adde5e6f4eb00555642d6
ncnc8388 <109010398+ncnc8388@users.noreply.github.com>
2025-08-08 18:36:00 +0800

    Update iptv.py

--- .github/workflows/iptv.py
+++ .github/workflows/iptv.py
@@ -312,7 +312,7 @@ if __name__ == "__main__":
     # IPTV 文件 URL（您可以添加自己的文件 URL 列表）
     file_urls = [
         "https://fy.188766.xyz/?ip=192.168.1.2&bconly=true",
-        "http://1303157606-jbnue8hg1f.ap-guangzhou.tencentscf.com/?url=https://fy.188766.xyz/?ip=192.168.1.2&bconly=true",
+        "https://1303157606-jbnue8hg1f.ap-guangzhou.tencentscf.com/?url=https://fy.188766.xyz/?ip=192.168.1.2&bconly=true",
         "https://raw.githubusercontent.com/ncnc8388/ncnc8388.github.io/refs/heads/main/河南电信.m3u",
         #"https://raw.githubusercontent.com/vbskycn/iptv/refs/heads/master/tv/iptv4.m3u",
         "https://raw.githubusercontent.com/q1017673817/iptvz/refs/heads/main/组播_四川电信.txt",
@@ -344,6 +344,7 @@ if __name__ == "__main__":
     # 执行主函数
 
     asyncio.run(main(file_urls, cctv_channel_file, province_channel_files))
+
 
 
 
