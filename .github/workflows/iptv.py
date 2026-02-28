4795bbb2012c9c6a1f1720c7ddd22bf8f1a2e69e
ncnc8388 <109010398+ncnc8388@users.noreply.github.com>
2026-02-28 13:12:33 +0800

    Update iptv.py

--- .github/workflows/iptv.py
+++ .github/workflows/iptv.py
@@ -329,8 +329,9 @@ if __name__ == "__main__":
         "https://raw.githubusercontent.com/q1017673817/iptvz/refs/heads/main/组播_河北电信.txt",
         "https://raw.githubusercontent.com/q1017673817/iptvz/refs/heads/main/组播_甘肃电信.txt",
         "https://raw.githubusercontent.com/q1017673817/iptvz/refs/heads/main/组播_湖南电信.txt",
-        "https://raw.githubusercontent.com/wokaotianshi123/zubotv/refs/heads/main/hunanzubo.txt",
-        "https://raw.githubusercontent.com/kakaxi-1/zubo/refs/heads/main/itvlist.txt",
+        "https://raw.githubusercontent.com/wokaotianshi123/zubotv/refs/heads/main/sichuanzubo.txt",
+        #"https://raw.githubusercontent.com/kakaxi-1/zubo/refs/heads/main/itvlist.txt",
+       "https://raw.githubusercontent.com/lisa3456/zubo/refs/heads/main/IPTV.txt",
         #"https://raw.githubusercontent.com/q1017673817/iptvz/refs/heads/main/组播_福建电信.txt",
         #"https://raw.githubusercontent.com/q1017673817/iptvz/refs/heads/main/组播_河南电信.txt",
         "https://raw.githubusercontent.com/ncnc8388/zhiyongm3u/refs/heads/main/.github/workflows/IPTV/ITV.txt",
@@ -359,6 +360,7 @@ if __name__ == "__main__":
     # 执行主函数
 
     asyncio.run(main(file_urls, cctv_channel_file, province_channel_files))
+
 
 
 
